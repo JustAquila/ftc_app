5633b8d5a4ca026134ae1f2830c88ceef0c011a4
Liviu-Constantin Olaru <liviu.constantin.olaru@gmail.com>
2017-01-14 15:41:03 +0200

    Program pentru controlarea ambelor stickuri

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test2.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+//program scris pentru Xbox 360 de la Microsoft
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="Test2", group="Aquila")
+public class Test2 extends LinearOpMode
+{
+    private DcMotor motorFrontLeft;
+    private DcMotor motorFrontRight;
+    private DcMotor motorBackLeft;
+    private DcMotor motorBackRight;
+
+    @Override
+    public void runOpMode () throws InterruptedException
+    {
+
+        motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
+        motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
+        motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
+        motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
+
+        motorFrontLeft.setDirection(DcMotor.Direction.REVERSE);
+        motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
+
+        waitForStart();
+
+        while(opModeIsActive())
+        {
+            motorFrontLeft.setPower(-gamepad1.left_stick_y);
+            motorFrontRight.setPower(-gamepad1.right_stick_y);
+            motorBackLeft.setPower(-gamepad1.left_stick_y);
+            motorBackRight.setPower(-gamepad1.right_stick_y);
+            idle();
+        }
+    }
+}
